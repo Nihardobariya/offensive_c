df6c4c015044b13415d7766a03ffadf1edc918a0
Nihardobariya <41757004+Nihardobariya@users.noreply.github.com>
2021-01-31 23:50:47 +0530

    Add files via upload

---
+++ CopyFileFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CopyFileFunction
+{
+    class Program
+    {
+
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool CopyFile(String lpExistingFileName, String lpNewFileName, bool bFailIfExists);
+
+
+        static void Main(string[] args)
+        {
+            if(CopyFile("fileone.txt", "filetwo.txt", true))
+            {
+                Console.WriteLine("File successfully copied");
+            }
+            else
+            {
+                Console.WriteLine("File Copying stopped");
+            }
+        }
+    }
+}

